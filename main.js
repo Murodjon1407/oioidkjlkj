09ef06c5846265094b892d926426872d7c81b3df
Murodjon1407 <murodjonshamshumetov@gmail.com>
2024-12-19 16:09:38 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+const oquvchilar = [1,2,3,4,5,88,43,22,77,33,99]
+
+for(let index = 0; index < oquvchilar.length;index++){
+    console.log(oquvchilar[index]);
+    
+
+    if(index % 2 ==0){
+        console.log(`toq sonlar : ${oquvchilar[index]}`);
+    }else{
+        console.log(`juft sonlar :${oquvchilar[index]}`);
+    }
+}
+
+
+
